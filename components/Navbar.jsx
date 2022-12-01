47a64067546e03b50afde720a62fd913f131ad07
mikemtzp <mikemp8226@gmail.com>
2022-12-01 13:02:27 -0600

    Add desktop navbar sections

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,16 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
+  const navSections = [
+    ['Home', '/'],
+    ['About', '/#about'],
+    ['Skills', '/#skills'],
+    ['Projects', '/#projects'],
+    ['Contact', '/#contact'],
+  ];
+
   return (
     <div className='fixed w-full h-16 shadow-xl z-[100] border-b border-slate-800'>
       <div className='flex justify-between items-center w-full h-full px-4 2xl:px-14'>
@@ -15,6 +24,18 @@ const Navbar = () => {
             className='relative rounded-lg w-32 h-auto'
             priority
           />
+        </div>
+        <div>
+          <ul className='flex'>
+            {navSections.map(([title, url]) => (
+              <Link href={url} key={title} className='ml-4 relative group'>
+                <div className='absolute -inset-0.5 rounded-lg -z-10 opacity-0 transition duration-700 group-hover:opacity-75 group-hover:transition bg-gradient-to-tr from-purple-600 to-green-600'></div>
+                <li className='bg-black text-sm text-gray-300 rounded-md p-1'>
+                  {title}
+                </li>
+              </Link>
+            ))}
+          </ul>
         </div>
       </div>
     </div>

04f0518d08091d3cca2edcd429320c1e8d1dca8f
mikemtzp <mikemp8226@gmail.com>
2022-12-11 10:24:23 -0600

    Add basic layout + title

---
+++ components/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div id='contact' className='w-full lg:h-screen'>
+      <div className='max-w-[1240px] mx-auto py-16 w-full'>
+      <p className='uppercase gradient-text tracking-widest pl-5 xl:pl-0'>
+        Contact
+      </p>
+      <h2 className='pl-5 xl:pl-0 mb-3 sm:mb-5'>Get in touch</h2>
+
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import About from '../components/About';
+import Contact from '../components/Contact';
 import Main from '../components/Main';
 import Navbar from '../components/Navbar';
 import Projects from '../components/Projects';
@@ -20,6 +21,7 @@ export default function Home() {
       <About />
       <Skills />
       <Projects />
+      <Contact />
     </div>
   );
 }

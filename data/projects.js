a271926547325cf156be8cfd987e74a0f2501845
mikemtzp <mikemp8226@gmail.com>
2022-12-12 16:06:07 -0600

    Add project temporal link

--- data/projects.js
+++ data/projects.js
@@ -10,37 +10,37 @@ const projects = [
     title: 'Easy Fly',
     image: easyfly,
     tech: 'React/RoR',
-    projectUrl: '/easyfly',
+    projectUrl: 'https://github.com/mikemtzp/Easy-Fly',
   },
   {
     title: 'Budjet',
     image: budjet,
     tech: 'Ruby on Rails',
-    projectUrl: '/budjet',
+    projectUrl: 'https://github.com/mikemtzp/Budget_app',
   },
   {
     title: 'Dream Picture',
     image: dreampicture,
     tech: 'React',
-    projectUrl: '/dreampicture',
+    projectUrl: 'https://github.com/mikemtzp/dream-picture',
   },
   {
     title: 'Recipe App',
     image: recipeapp,
     tech: 'Ruby on Rails',
-    projectUrl: '/recipeapp',
+    projectUrl: 'https://github.com/mikemtzp/Recipe_app',
   },
   {
     title: 'Space Traveler\'s Hub',
     image: spacetravelershub,
     tech: 'React/Redux',
-    projectUrl: '/spacetravelershub',
+    projectUrl: 'https://github.com/mikemtzp/space-travelers-hub',
   },
   {
     title: 'Awesome Books',
     image: awesomebooks,
     tech: 'JavaScript',
-    projectUrl: '/awesomebooks',
+    projectUrl: 'https://github.com/mikemtzp/Awesome-books-es6',
   },
 ];
 
